f4f130f982551c642ac00d9b90a49bd6e3d14063
江钟权 <edwardubuntu@gmail.com>
2020-03-05 16:29:59 +0800

    Change the test not belong here.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,10 @@ import 'package:myanimate/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AnimatedContainerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the bottom icon and trigger a animation.
+    await tester.tap(find.byIcon(Icons.play_arrow));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
